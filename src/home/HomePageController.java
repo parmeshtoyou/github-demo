9307587c54101103c354c88f434273097da7c6dc
Parmesh <Parmeshtoyou@gmail.com>
2021-01-13 23:26:12 +0530

    added call to fetch item list

--- src/home/HomePageController.java
+++ src/home/HomePageController.java
@@ -4,4 +4,8 @@ public class HomePageController {
     void homeController() {
         System.out.println("Welcome to the home page");
     }
+
+    void getItemList() {
+        System.out.println("Getting item list");
+    }
 }
