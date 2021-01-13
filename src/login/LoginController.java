2d82ad4b6229eb58604c3640790ad1c5a170159e
Parmesh <Parmeshtoyou@gmail.com>
2021-01-13 23:28:19 +0530

    added logout

--- src/login/LoginController.java
+++ src/login/LoginController.java
@@ -4,4 +4,8 @@ public class LoginController {
     void doLogin() {
         System.out.println("dummy function of login which is the part of feature-1 branch");
     }
+
+    void doLogout() {
+        System.out.println("dummy function to logout which is the part of feature-1 branch commit-2");
+    }
 }
